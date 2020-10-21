40df595a1dd358cfde3bc27bc14b3428e8d468af
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-21 15:14:18 -0300

    Primeiro teste

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe('TESTANDO ALGUMA COISA', () => {
+  it('should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe("TESTANDO OUTRA COISA", () => {
+    test('should return Ronaldo', () => {
+        const nome = 'Ronaldo';
+        expect(nome).toBe('Ronaldo');
+    });
+})
