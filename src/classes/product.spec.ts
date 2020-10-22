e908d392c2f6b966930a5d6c610c1e61f8dc657d
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-21 21:43:50 -0300

    Product

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,16 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = createSut('Camiseta', 49.9);
+    expect(sut).toHaveProperty('name', 'Camiseta');
+    expect(sut).toHaveProperty('price', 49.9);
+    expect(sut.price).toBeCloseTo(49.9);
+  });
+});
