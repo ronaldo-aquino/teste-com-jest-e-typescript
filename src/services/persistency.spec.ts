0b5daa954aff0aa4fbd6c8af140c9558c0d0bb72
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-21 21:05:19 -0300

    Primeiro teste com spyOn

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,13 +1,31 @@
-describe('TESTANDO ALGUMA COISA', () => {
-  it('should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
+import { Persistency } from './persistency';
+
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should return undefined', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log onse', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
-
-describe("TESTANDO OUTRA COISA", () => {
-    test('should return Ronaldo', () => {
-        const nome = 'Ronaldo';
-        expect(nome).toBe('Ronaldo');
-    });
-})
