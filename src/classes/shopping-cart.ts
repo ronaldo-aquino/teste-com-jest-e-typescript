b7725c313b8d776f62dbcd1f6139e3c0923f99c6
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-22 21:12:39 -0300

    Shipping cart com mock

--- src/classes/shopping-cart.ts
+++ src/classes/shopping-cart.ts
@@ -3,38 +3,38 @@ import { Discount } from './discount';
 import { ShoppingCartProtocol } from './interfaces/shopping-cart-protocol';
 
 export class ShoppingCart implements ShoppingCartProtocol {
-  private readonly _items: CartItem[] = [];
+    private readonly _items: CartItem[] = [];
 
-  constructor(private readonly discount: Discount) {}
+    constructor(private readonly discount: Discount) {}
 
-  addItem(item: CartItem): void {
-    this._items.push(item);
-  }
+    addItem(item: CartItem): void {
+        this._items.push(item);
+    }
 
-  removeItem(index: number): void {
-    this._items.splice(index, 1);
-  }
+    removeItem(index: number): void {
+        this._items.splice(index, 1);
+    }
 
-  get items(): Readonly<CartItem[]> {
-    return this._items;
-  }
+    get items(): Readonly<CartItem[]> {
+        return this._items;
+    }
 
-  total(): number {
-    return +this._items
-      .reduce((total, next) => total + next.price, 0)
-      .toFixed(2);
-  }
+    total(): number {
+        return +this._items
+            .reduce((total, next) => total + next.price, 0)
+            .toFixed(2);
+    }
 
-  totalWithDicount(): number {
-    return this.discount.calculate(this.total());
-  }
+    totalWithDicount(): number {
+        return this.discount.calculate(this.total());
+    }
 
-  isEmpty(): boolean {
-    return this._items.length === 0;
-  }
+    isEmpty(): boolean {
+        return this._items.length === 0;
+    }
 
-  clear(): void {
-    console.log('Carrinho de compras foi limpo...');
-    this._items.length = 0;
-  }
+    clear(): void {
+        console.log('Carrinho de compras foi limpo...');
+        this._items.length = 0;
+    }
 }
